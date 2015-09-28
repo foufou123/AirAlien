410a845665f07dc718e1cf00dac22b331c32816c
Raouf Nabi <raouf.nabi@gmail.com>
2015-09-27 20:06:22 -0400

    fixed facebook image instead of gravatar when available

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,11 @@ module ApplicationHelper
 
 	def avatar_url(user)
 		gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+		if user.image
+			user.image
+		else
 		"https://www.gravatar.com/avatar/#{gravatar_id}"
-	end
-
-end
+	    end
+  end
+ end
+ 
